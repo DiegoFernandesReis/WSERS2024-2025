9a1f3c1593a7f5e506cc7d3f61d59b82b7808a98
Your Name <you@example.com>
2024-10-02 13:06:08 +0200

    2024

---
+++ Shop/home.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <title>Document</title>
+    <style>
+        p {
+            background-color: lightblue;
+        }
+        </style>
+</head>
+<body>
+    <p>Hello, and welcome to my page</p>
+
+    <?php
+    print("Hello world");
+    ?>
+</body>
+</html>

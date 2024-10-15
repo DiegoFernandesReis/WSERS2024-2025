b5071a2dcdeaf620c05ca2ca106472370a13ed8f
Your Name <you@example.com>
2024-10-15 13:38:10 +0200

    2024

--- test/pages/Commoncode.php
+++ test/pages/Commoncode.php
@@ -7,15 +7,15 @@ function NavigationBar ($buttontohighlight) {
     <a href="Home.php"Home<?php if ($buttontohighlight == "home"){
                                    print ("class='active'");
         
-    } ?>>home</a>
+    } ?>>Home</a>
     <a href="Nintendo switch.php"Nintendo switch <?php if ($buttontohighlight == "nintendoswitch"){
                                    print ("class='active'");
         
-    } ?>>nintendoswitch</a>
+    } ?>>Nintendoswitch</a>
     <a href="Controller.php"Pro Controller <?php if ($buttontohighlight == "controller"){
                                    print ("class='active'");
         
-    } ?>>controller</a>
+    } ?>>Controller</a>
     <a href="Accesories.php"Accesories <?php if ($buttontohighlight == "Accesories"){
                                    print ("class='active'");
         

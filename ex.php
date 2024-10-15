93ddadc22ca0f9ee49c230624cfa32fb212e60e8
Your Name <you@example.com>
2024-10-15 15:02:19 +0200

    2024

---
+++ ex.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Document</title>
+</head>
+<body>
+    <?php
+    $array=[];
+    $array.push($a,10,20,30);
+    $array.pop();
+    for($i=0; $i<count($a);$i++){
+        print($a);
+    }
+    ?>
+</body>
+</html>

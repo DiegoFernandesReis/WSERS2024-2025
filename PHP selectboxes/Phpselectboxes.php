7954c18c01ccd7144b889687262c833b011f9bd7
Your Name <you@example.com>
2024-10-22 14:12:20 +0200

    2024

--- PHP selectboxes/Phpselectboxes.php
+++ PHP selectboxes/Phpselectboxes.php
@@ -7,14 +7,29 @@
 </head>
 <body>
     <?php
+
+    $carsArray = ["Volvo", "Saab", "Opel", "Audi", "BMW", "Dacia"];
+    //$carsArrayAssoc = array("a" => "Ford", "b" => "Saab", "c"=> "Opel");
+
     if(isset($_POST["Username"])){
         print("Welcome". $_POST["Username"]);
     }
     ?>
     <h1>Login form</h1>
-    <form method="POST" action="">
+    <form method="POST">
         Please enter your name: <input type="text name="UserName">
         Please enter your password : <input type="password" name="Password>
+
+        <select name="cars" >
+            <?php
+            foreach ($carsArray as $val){
+                ?>
+                 <option value="<?php echo($val); ?>"><?php echo ($val); ?></option>
+                <?php
+            }
+            ?>
+           
+        </select>
         <input type="submit" value="send">
     </form>
     <a href="Phpselectboxes.php">reset the page</a>

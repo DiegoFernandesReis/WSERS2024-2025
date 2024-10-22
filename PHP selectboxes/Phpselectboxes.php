bfb119c51c4156340512f58078bda653659ca228
Your Name <you@example.com>
2024-10-22 13:37:25 +0200

    2024

--- PHP selectboxes/Phpselectboxes.php
+++ PHP selectboxes/Phpselectboxes.php
@@ -7,12 +7,14 @@
 </head>
 <body>
     <?php
-    if(isset($_GET["Username"])){
-        print("Welcome". $_GET["Username"]);
+    if(isset($_POST["Username"])){
+        print("Welcome". $_POST["Username"]);
     }
     ?>
-    <form method="GET" action="">
+    <h1>Login form</h1>
+    <form method="POST" action="">
         Please enter your name: <input type="text name="UserName">
+        Please enter your password : <input type="password" name="Password>
         <input type="submit" value="send">
     </form>
     <a href="Phpselectboxes.php">reset the page</a>

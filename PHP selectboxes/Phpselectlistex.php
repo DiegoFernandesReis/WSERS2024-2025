6a61aed5feba03aee95efc48f225c00cb86fed18
Your Name <you@example.com>
2024-10-23 13:40:22 +0200

    2024

---
+++ PHP selectboxes/Phpselectlistex.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php
+    $arraycolor=["red","green","blue"];
+    ?>
+
+<select name="color">
+<?php
+            foreach ($arraycolor as $val){
+                ?>
+                 <option value="<?=$val?>"<?php if(isset($_POST["color"])){
+                    if($_POST["color"] == $val){
+                        print("selected='selected'");
+                    }
+                 }?>><?=$val?></option>
+
+                 <?="You have Selected: . $val"?>
+                <?php
+            }
+            ?>
+           
+    
+</select>
+<input type="submit" value="send"> 
+</body>
+</html>

fc3ee4ead3d87a5750eca68a4b46f96a77105101
Your Name <you@example.com>
2024-10-23 15:13:49 +0200

    2024

--- FileHandling/TextRead.php
+++ FileHandling/TextRead.php
@@ -10,6 +10,7 @@
         .AllProducts {
             display: flex;
             justify-content: space-around;
+ 
         }
  
         .OneProduct {
@@ -25,24 +26,26 @@
     <div class="AllProducts">
         <?php
         $myFile = fopen("Products.csv", "r");
+        $line = fgets($myFile);
         while (!feof($myFile)) {
             $line = fgets($myFile);
             $arrayOfPieces = explode(";", $line);
             //print("<div>" . $line . "</div>");
+            if (count($arrayOfPieces) == 6); {
+ 
+ 
         ?>
  
  
-            <div class="OneProduct">
-                <div> <?= $arrayOfPieces[1]?></div>
-                <div> <?= $arrayOfPieces[2]?></div>
-                <img src="./ <?= $arrayOfPieces[5] ?>">
-                <div> <?= $arrayOfPieces[3]?></div>
-                <div>Inventory: <?= $arrayOfPieces[4]?></div>
-            </div>
- 
- 
- 
+                <div class="OneProduct">
+                    <div> <?= $arrayOfPieces[1] ?></div>
+                    <div> <?= $arrayOfPieces[2] ?></div>
+                    <img src="./<?= $arrayOfPieces[5] ?>" width="100px">
+                    <div> <?= $arrayOfPieces[3] ?></div>
+                    <div>Inventory: <?= $arrayOfPieces[4] ?></div>
+                </div>
         <?php
+            }
         }
         ?>
     </div>

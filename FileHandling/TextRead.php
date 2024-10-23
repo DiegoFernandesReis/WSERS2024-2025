14a2fa0744201b821fd0fbe3a29e425d78634be7
Your Name <you@example.com>
2024-10-23 15:02:00 +0200

    2024

---
+++ FileHandling/TextRead.php
@@ -0,0 +1,54 @@
+<!DOCTYPE html>
+<html lang="en">
+ 
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+ 
+    <style>
+        .AllProducts {
+            display: flex;
+            justify-content: space-around;
+        }
+ 
+        .OneProduct {
+            border: 1px solid red;
+            display: flex;
+            justify-content: center;
+            flex-direction: column;
+        }
+    </style>
+</head>
+ 
+<body>
+    <div class="AllProducts">
+        <?php
+        $myFile = fopen("Products.csv", "r");
+        while (!feof($myFile)) {
+            $line = fgets($myFile);
+            $arrayOfPieces = explode(";", $line);
+            //print("<div>" . $line . "</div>");
+        ?>
+ 
+ 
+            <div class="OneProduct">
+                <div> <?= $arrayOfPieces[1]?></div>
+                <div> <?= $arrayOfPieces[2]?></div>
+                <img src="./ <?= $arrayOfPieces[5] ?>">
+                <div> <?= $arrayOfPieces[3]?></div>
+                <div>Inventory: <?= $arrayOfPieces[4]?></div>
+            </div>
+ 
+ 
+ 
+        <?php
+        }
+        ?>
+    </div>
+ 
+ 
+ 
+</body>
+ 
+</html>

aebf015f62ff02436357e90b3ce4bf2b1f0560a9
Your Name <you@example.com>
2024-11-06 13:16:02 +0100

    2024

--- test/pages/Accesories.php
+++ test/pages/Accesories.php
@@ -11,5 +11,31 @@
     include_once("Commoncode.php");
     NavigationBar("Accesories");
     ?>
+    <div class="AllProducts">
+        <?php
+        $myFile = fopen("Accessories.csv", "r");
+        $line = fgets($myFile);
+        while (!feof($myFile)) {
+            $line = fgets($myFile);
+            $arrayOfPieces = explode(";", $line);
+            //print("<div>" . $line . "</div>");
+            if (count($arrayOfPieces) == 6) {
+ 
+ 
+        ?>
+ 
+ 
+                
+                    <div> <?= $arrayOfPieces[1] ?></div>
+                    <img src="../images/<?= $arrayOfPieces[5] ?>" width="400px">
+                    <div class="OneProduct"> <?= $arrayOfPieces[2] ?></div>
+                    <div class="OneProduct"> <?= $arrayOfPieces[3] ?></div>
+                   <div class="OneProduct">Inventory: <?= $arrayOfPieces[4] ?></div>
+                
+        <?php
+            }
+        }
+        ?>
+    </div>
 </body>
 </html>

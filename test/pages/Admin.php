773287da81da672fb134e54cd4b5f734e6ba78b9
Your Name <you@example.com>
2024-11-20 13:57:49 +0100

    2024

--- test/pages/Admin.php
+++ test/pages/Admin.php
@@ -10,12 +10,17 @@
     include_once("Commoncode.php");
     NavigationBar("Admin");
 
-
+    
     ?>
 
-    <h1>Add Items:</h1>
+   
     <form method="POST">
+    <h1>Add Items:</h1>
     <input type="text" name="item" placeholder="Enter a Item" />
+    <h1>Price:</h1>
+    <input type="text" name="price" placeholder ="Enter a price"/>
+    <h1>Description:</h1>
+    <input type="text" name="description" placeholder="Enter a description"/>
     <input type="button" value="add"/>
     </form>
 

cf324dbd437e19fbf95399ef41ae4b335c1b878d
Diego Fernandes Reis <ferdi888@school.lu>
2024-12-17 15:14:03 +0100

    2024

--- test/pages/basket.php
+++ test/pages/basket.php
@@ -10,12 +10,29 @@
     include_once("Commoncode.php");
     NavigationBar("busket");
     
-
+    if(isset($_POST['Clear Button'])){
+        $myFile=fopen("basket.csv", "w");
+        fwrite($myfile, "list:");
+        fclose($myFile);
+    }
     ?>
     <div class="AllProducts">
         <?php
-        $myFile = fopen("basket.csv" , "r");
-        $line = fgets($myFile);
+        if(!isset($_SESSION["cart"])){
+            $_SESSION["cart"] = [];
+        }
+        if(isset($_POST["buy"])){
+            array_push($_SESSION["cart"], $_POST["boughtitem"], $_POST["Price"])
+            var_dump($_SESSION);
+        }
+
+       
+
+
+
+
+        //$myFile = fopen("basket.csv" , "r");
+        /*$line = fgets($myFile);
         while (!feof($myFile)) {
             $line = fgets($myFile);
             $arrayOfPieces = explode(";", $line);
@@ -23,8 +40,8 @@
             if (count($arrayOfPieces) == 3) {
  
         ?>
- 
- 
+    
+       
                     <div> <?=($_SESSION["language"] == "EN") ?$arrayOfPieces[0]:$arrayOfPieces[6] ?></div>
                     <?php  if(isset($_SESSION["usertype"]) && $_SESSION["usertype"] == 2) {
                         
@@ -36,8 +53,13 @@
 
                 <?php
             }
-        }
+            
+        }*/
         ?>
+        <script>window.location=window.location.href</script>
+        <form method="POST">
+       <input type="submit" value="Clear Button" name="Clear Button">
+       </form>
        
       
     

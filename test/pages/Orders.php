ace23f20aafb5dcdd08ebda0379d3a93f070192c
Your Name <you@example.com>
2024-12-18 15:12:00 +0100

    2024

---
+++ test/pages/Orders.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<link rel="stylesheet" href="../style.css?<?=time()?>">
+    <img src="../images/nintendo_banner.webp" id="image">
+    <meta charset="UTF-8">
+    <title>Document</title>
+</head>
+<body>
+    <?php
+    include_once("Commoncode.php");
+    NavigationBar("orders");
+
+    $myFile = fopen("Orders.csv" , "r");
+    $line = fgets($myFile);
+    while (!feof($myFile)) {
+        $line = fgets($myFile);
+        $arrayOfPieces = explode(";", $line);
+        //print("<div>" . $line . "</div>");
+        if (count($arrayOfPieces) == 3) {
+
+    ?>
+      <div> <?=($_SESSION["language"] == "EN") ?$arrayOfPieces[0]:$arrayOfPieces[6] ?></div>
+                    <?php  if(isset($_SESSION["usertype"]) && $_SESSION["usertype"] == 2) {
+                        
+                        ?>
+                        <div class="Navcenter"><?=$arrayOfPieces[1] ?></div><?php
+                    }
+                    ?>
+                    <div class="OneProduct"><?=$arrayOfPieces[2] ?></div>
+                    <?php
+        }
+    }
+                    ?>
+                
+                
+</body>
+</html>

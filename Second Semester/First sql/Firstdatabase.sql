700fead70e56a62b9d53fcc06016ced27851c396
Your Name <you@example.com>
2025-02-25 15:12:49 +0100

    2005

--- Second Semester/First sql/Firstdatabase.sql
+++ Second Semester/First sql/Firstdatabase.sql
@@ -5,6 +5,7 @@ use myDbTestNo2;
 create table Countries (
     CountryId int primary key auto_increment,
     CountryName varchar(255)
+    
 );
 
 
@@ -12,15 +13,15 @@ CREATE TABLE Users (
     UserId int  primary key auto_increment,
     Name varchar(255),
     Email varchar(255),
-    CountryId int
-    FOREIGN KEY (CountryId) REFERENCES Countries (CountryId)
+    CountryId int,
+    FOREIGN KEY (CountryId) REFERENCES Countries(CountryId)
 );
 
-Insert into Countries(CountryName) Values('Luxembourg')
-Insert into Countries(CountryName) Values('Portugal')
-Insert into Countries(CountryName) Values('Germany')
-Insert into Countries(CountryName) Values('Yemen')
+Insert into Countries (CountryName) Values("Luxembourg");
+Insert into Countries (CountryName) Values("Portugal");
+Insert into Countries (CountryName) Values("Germany");
+Insert into Countries (CountryName) Values("Yemen");
 
-Insert into Users(Name,Email,CountryId) Values("Dan","dude@lpem", 5)
-Insert into Users(Name,Email,CountryId) Values("Ayman","ayman@lpem", 4)
-Insert into Users(Name,Email,CountryId) Values("Diego","diego@lpem", 1)
+Insert into Users (Name,Email,CountryId) Values("Dan","dude@lpem", 4);
+Insert into Users (Name,Email,CountryId) Values("Ayman","ayman@lpem", 3);
+Insert into Users (Name,Email,CountryId) Values("Diego","diego@lpem", 2);

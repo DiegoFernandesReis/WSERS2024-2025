5c1fc7045bbcf5e82cd5015ef0c9c3dfcc5a934f
Your Name <you@example.com>
2025-02-25 14:50:07 +0100

    2024

---
+++ Second Semester/First sql/Firstdatabase.sql
@@ -0,0 +1,26 @@
+drop database if exists myDbTestNo2;
+create database myDbTestNo2;
+use myDbTestNo2;
+
+create table Countries (
+    CountryId int primary key auto_increment,
+    CountryName varchar(255)
+);
+
+
+CREATE TABLE Users (
+    UserId int  primary key auto_increment,
+    Name varchar(255),
+    Email varchar(255),
+    CountryId int
+    FOREIGN KEY (CountryId) REFERENCES Countries (CountryId)
+);
+
+Insert into Countries(CountryName) Values('Luxembourg')
+Insert into Countries(CountryName) Values('Portugal')
+Insert into Countries(CountryName) Values('Germany')
+Insert into Countries(CountryName) Values('Yemen')
+
+Insert into Users(Name,Email,CountryId) Values("Dan","dude@lpem", 5)
+Insert into Users(Name,Email,CountryId) Values("Ayman","ayman@lpem", 4)
+Insert into Users(Name,Email,CountryId) Values("Diego","diego@lpem", 1)
